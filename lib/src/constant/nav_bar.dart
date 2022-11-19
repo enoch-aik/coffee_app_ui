37ec1e688e2041ce97f23713afdca7efb6b082de
Enoch Aikpokpodion <enoch_aik@icloud.com>
2022-11-19 02:51:35 +0100

    added bottom nav icons

---
+++ lib/src/constant/nav_bar.dart
@@ -0,0 +1,21 @@
+import 'package:coffee_app_ui/src/screens/home/screen.dart';
+import 'package:coffee_app_ui/src/widgets/custom_nav_icons.dart';
+import 'package:flutter/widgets.dart';
+
+final List<Widget> hostBottomNavBody = [
+  const HomeScreen(),
+  const HomeScreen(),
+  const HomeScreen(),
+  const HomeScreen(),
+  const HomeScreen(),
+];
+
+List<BottomNavigationBarItem> bottomNavItems = const [
+  BottomNavigationBarItem(icon: Icon(NavBarIcons.home), label: 'Home',activeIcon: Icon(NavBarIcons.homeFilled)),
+  BottomNavigationBarItem(
+      icon: Icon(NavBarIcons.favourite), label: 'Favourite',activeIcon: Icon(NavBarIcons.favouriteFilled)),
+  BottomNavigationBarItem(icon: Icon(NavBarIcons.cart), label: 'Cart',activeIcon: Icon(NavBarIcons.cartFilled)),
+  BottomNavigationBarItem(
+      icon: Icon(NavBarIcons.notification), label: 'Notification',activeIcon: Icon(NavBarIcons.notificationFilled)),
+  BottomNavigationBarItem(icon: Icon(NavBarIcons.profile), label: 'Profile',activeIcon: Icon(NavBarIcons.profileFilled)),
+];
---
+++ lib/src/widgets/custom_nav_icons.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+
+class NavBarIcons {
+  NavBarIcons._();
+
+  static const _kFontFam = 'NavBarIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData notification =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData notificationFilled =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profile =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profileFilled =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cart =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cartFilled =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData favourite =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData favouriteFilled =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homeFilled =
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
