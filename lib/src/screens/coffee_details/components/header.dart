c14b260a87f8974ea7d7bf7ef40480d42eece782
Enoch Aikpokpodion <enoch_aik@icloud.com>
2022-11-19 19:20:00 +0100

    added description, choice of chocolate and size

---
+++ lib/src/screens/coffee_details/components/header.dart
@@ -0,0 +1,134 @@
+import 'dart:ui';
+
+import 'package:coffee_app_ui/exports.dart';
+import 'package:flutter_svg/svg.dart';
+
+Widget coffeeDetailsHeader(){
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 20.h),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(40.r),
+      child: SizedBox(
+        width: 350.w,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/espresso_large.png',
+              fit: BoxFit.fitWidth,
+              filterQuality: FilterQuality.high,
+            ),
+            Positioned(
+                left: 11.w,
+                top: 22.h,
+                child: SvgPicture.asset('assets/svg/back_icon.svg')),
+            Positioned(
+                right: 11.w,
+                top: 22.h,
+                child: SvgPicture.asset('assets/svg/like_icon.svg')),
+            //COFFEE DETAILS WITH  BACKDROP FILTER
+
+            Positioned(
+              bottom: 0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.r),
+                child: SizedBox(
+                  //height: 130.h,
+                  width: 350.w,
+                  child: BackdropFilter(
+                    filter:
+                    ImageFilter.blur(sigmaX: 7.5, sigmaY: 7.5),
+                    child: Container(
+                      color: CustomColors.black.withOpacity(0.3),
+                      padding:
+                      EdgeInsets.fromLTRB(27.w, 25.h, 26.w, 18),
+                      child: Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: [
+                              KText(
+                                'Espresso',
+                                fontSize: 24.sp,
+                                color: CustomColors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              SizedBox(height: 2.h),
+                              KText('with Chocolate',
+                                  fontSize: 12.sp,
+                                  color: CustomColors.white
+                                      .withOpacity(0.8)),
+                              SizedBox(height: 21.h),
+                              Row(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: CustomColors.starColor,
+                                    size: 17,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 5.w),
+                                    child: KText(
+                                      '4.8',
+                                      fontSize: 16.sp,
+                                      fontWeight: FontWeight.w500,
+                                      color: CustomColors.white,
+                                    ),
+                                  ),
+                                  KText('(6,098)',
+                                      color: CustomColors.white
+                                          .withOpacity(0.8),
+                                      fontSize: 10.sp)
+                                ],
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/svg/coffee_icon.svg',
+                                    width: 38.w,
+                                  ),
+                                  Padding(
+                                    padding:
+                                    EdgeInsets.only(left: 30.w),
+                                    child: SvgPicture.asset(
+                                      'assets/svg/chocolate_icon.svg',
+                                      width: 58.w,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 5.h),
+                                child: KText(
+                                  'Medium Roasted',
+                                  color: CustomColors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14.sp,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/src/screens/coffee_details/screen.dart
@@ -0,0 +1,200 @@
+import 'package:coffee_app_ui/exports.dart';
+import 'package:coffee_app_ui/src/constant/coffee.dart';
+import 'package:coffee_app_ui/src/screens/coffee_details/components/header.dart';
+import 'package:expandable_text/expandable_text.dart';
+
+class CoffeeDetailsScreen extends StatelessWidget {
+  const CoffeeDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ValueNotifier<int> selectedChocolate = ValueNotifier(0);
+    return KScaffold(
+      backgroundColor: CustomColors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //HEADER IMAGE
+            coffeeDetailsHeader(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15.h),
+                    child: Text(
+                      'Description',
+                      style: GoogleFonts.inter(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w700,
+                          color: CustomColors.defaultTextColor),
+                    ),
+                  ),
+                  ExpandableText(
+                    coffeeDescription,
+                    expandText: 'Read More',
+                    collapseText: 'Show less',
+                    maxLines: 2,
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.inter(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w400,
+                        color: CustomColors.defaultTextColor),
+                    //linkColor: CustomColors.brown,
+                    linkStyle: GoogleFonts.inter(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w700,
+                        color: CustomColors.brown),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15.h, top: 25.h),
+                    child: Text(
+                      'Choice of Chocolate',
+                      style: GoogleFonts.inter(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w700,
+                          color: CustomColors.defaultTextColor),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ValueListenableBuilder(
+                valueListenable: selectedChocolate,
+                builder: (context, boxState, _) {
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 24.w),
+                        ChoiceChip(
+                          label: KText('White Chocolate',
+                              color: selectedChocolate.value == 0
+                                  ? CustomColors.white
+                                  : const Color(0xff777777)),
+                          selected: selectedChocolate.value == 0 ? true : false,
+                          selectedColor: CustomColors.brown,
+                          disabledColor: CustomColors.white,
+                          backgroundColor: CustomColors.white,
+                          onSelected: (value) {
+                            selectedChocolate.value = 0;
+                          },
+                          side: BorderSide(color: CustomColors.brown),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.w),
+                          child: ChoiceChip(
+                            label: KText('Milk Chocolate',
+                                color: selectedChocolate.value == 1
+                                    ? CustomColors.white
+                                    : const Color(0xff777777)),
+                            selected:
+                                selectedChocolate.value == 1 ? true : false,
+                            selectedColor: CustomColors.brown,
+                            disabledColor: CustomColors.white,
+                            backgroundColor: CustomColors.white,
+                            onSelected: (value) {
+                              selectedChocolate.value = 1;
+                            },
+                            side: BorderSide(color: CustomColors.brown),
+                          ),
+                        ),
+                        ChoiceChip(
+                          label: KText('Dark Chocolate',
+                              color: selectedChocolate.value == 2
+                                  ? CustomColors.white
+                                  : const Color(0xff777777)),
+                          selected: selectedChocolate.value == 2 ? true : false,
+                          selectedColor: CustomColors.brown,
+                          disabledColor: CustomColors.white,
+                          backgroundColor: CustomColors.white,
+                          onSelected: (value) {
+                            selectedChocolate.value = 2;
+                          },
+                          side: BorderSide(color: CustomColors.brown),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 15.h, top: 25.h),
+                        child: Text(
+                          'Size',
+                          style: GoogleFonts.inter(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                              color: CustomColors.defaultTextColor),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ChoiceChip(
+                            label: KText('S',
+                                color: selectedChocolate.value == 0
+                                    ? CustomColors.white
+                                    : const Color(0xff777777)),
+                            selected:
+                                selectedChocolate.value == 0 ? true : false,
+                            selectedColor: CustomColors.brown,
+                            disabledColor: CustomColors.white,
+                            backgroundColor: CustomColors.white,
+                            onSelected: (value) {
+                              selectedChocolate.value = 0;
+                            },
+                            side: BorderSide(color: CustomColors.brown),
+                          ),
+                          ChoiceChip(
+                            label: KText('M',
+                                color: selectedChocolate.value == 1
+                                    ? CustomColors.white
+                                    : const Color(0xff777777)),
+                            selected:
+                                selectedChocolate.value == 1 ? true : false,
+                            selectedColor: CustomColors.brown,
+                            disabledColor: CustomColors.white,
+                            backgroundColor: CustomColors.white,
+                            onSelected: (value) {
+                              selectedChocolate.value = 1;
+                            },
+                            side: BorderSide(color: CustomColors.brown),
+                          ),
+                          ChoiceChip(
+                            label: KText('L',
+                                color: selectedChocolate.value == 2
+                                    ? CustomColors.white
+                                    : const Color(0xff777777)),
+                            selected:
+                                selectedChocolate.value == 2 ? true : false,
+                            selectedColor: CustomColors.brown,
+                            disabledColor: CustomColors.white,
+                            backgroundColor: CustomColors.white,
+                            onSelected: (value) {
+                              selectedChocolate.value = 2;
+                            },
+                            side: BorderSide(color: CustomColors.brown),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
