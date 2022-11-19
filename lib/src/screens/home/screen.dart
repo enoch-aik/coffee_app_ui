dfe74509262b736f54ac41e57ed7278ed2193f02
Enoch Aikpokpodion <enoch_aik@icloud.com>
2022-11-19 02:00:22 +0100

    refactored code

--- lib/src/screens/home/screen.dart
+++ lib/src/screens/home/screen.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_app_ui/exports.dart';
 import 'package:coffee_app_ui/src/constant/coffee.dart';
 import 'package:coffee_app_ui/src/screens/home/components/coffee_card.dart';
+import 'package:coffee_app_ui/src/screens/home/components/special_card.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/tab_indicator.dart';
@@ -112,58 +113,7 @@ class HomeScreen extends StatelessWidget {
                     color: CustomColors.defaultTextColor,
                   ),
                 ),
-                Container(
-                  width: 329.w,
-                  height: 145.h,
-                  decoration: BoxDecoration(
-                      color: CustomColors.brown,
-                      borderRadius: BorderRadius.circular(25.r)),
-                  padding: EdgeInsets.all(10.w),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset('assets/images/latte.png'),
-                      SizedBox(
-                        width: 149.w,
-                        height: 103.h,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Special mixed and \nbrewed which you\nmust try!',
-                              style: GoogleFonts.inter(
-                                  color: CustomColors.white,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            Text.rich(
-                              TextSpan(
-                                  children: [
-                                    TextSpan(
-                                        text: '\$11.00  ',
-                                        style: defaultStyle.copyWith(
-                                            color: CustomColors.white,
-                                            fontWeight: FontWeight.w600,
-                                            fontFamily: GoogleFonts.inter()
-                                                .fontFamily)),
-                                    const TextSpan(
-                                      text: '\$20.3',
-                                    )
-                                  ],
-                                  style: defaultStyle.copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      color:
-                                          CustomColors.white.withOpacity(0.4),
-                                      fontFamily:
-                                          GoogleFonts.inter().fontFamily)),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )
+                specialCard()
               ],
             ),
           ),
